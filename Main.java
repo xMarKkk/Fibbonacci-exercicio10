69e26173b500079724bb0e529f26c925a0574edd
xMarKkk <148650610+xMarKkk@users.noreply.github.com>
2023-10-23 22:06:30 +0000

    FibbonacciAtv10

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ class Main {
   public static void main(String[] args) {
     int n1 = 1, n2 = 0;
 
-            System.out.println(num2);
-            System.out.println(num1);
+            System.out.println(n2);
+            System.out.println(n1);
 
             for(int i = 0; i < 8; i++){
                 n1 = n1 + n2;
