ca86e557630bfd6f12d5a65b5ad8f938d33a5116
xMarKkk <148650610+xMarKkk@users.noreply.github.com>
2023-10-23 00:56:35 +0000

    exercicio10

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int n1 = 1, n2 = 0;
+
+            System.out.println(num2);
+            System.out.println(num1);
+
+            for(int i = 0; i < 8; i++){
+                n1 = n1 + n2;
+                n2 = n1 - n2;
+                System.out.println(n1);
+            }
+  }
+}
